1344c4c56613ef84600f05a549ff28653ea33059
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-04 09:10:37 +0530

    Welcome

---
+++ src/main/java/com/bridgelab/AddressBookProblem/AddressBookMain.java
@@ -0,0 +1,15 @@
+package com.bridgelab.AddressBookProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBookMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome ");
+
+    }
+
+}
